8dab7c42c9327b6e5b8e6e61b7b323e38b6cd494
Lordpun <thedirtcart@gmail.com>
2026-07-03 16:31:17 -0400

    Added functions to get config data

--- config.hpp
+++ config.hpp
@@ -1,6 +1,23 @@
 #ifndef CONFIG_HPP
 #define CONFIG_HPP
 
+#include <filesystem>
+#include <vector>
+
+namespace fs = std::filesystem;
+
+struct sortPathData {
+  std::vector<std::string> extensions,
+  fs::path sortPath;
+};
+
+struct configData {
+  fs::path downloadsPath;
+  std::vector<sortPathData> sortPaths
+};
+
 int createConfig();
+
+configData getConfig();
 
 #endif
